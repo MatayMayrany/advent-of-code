0dca97183369caab9ccc67cf225cf11fd9f3a5b5
Matay Mayrany <mmayrany@invidi.com>
2022-12-15 16:54:38 +0100

    day 8

--- 2022/day-7-2022/day7.py
+++ 2022/day-7-2022/day7.py
@@ -1,46 +1,46 @@
 #! /usr/bin/env python
+from collections import defaultdict
+
+
 def build_map_of_directory_sizes(lines):
-    directory_sizes = {}
-    directory_parents = {}
-    current_directory = ''
-    current_size = 0
+    sizes = defaultdict(int)
+    path = []
     for line in lines:
         parts = line.split(' ')
-        if line[0] == '$':  # ignore ls
-            if parts[1] == 'cd':
-                if current_directory and current_directory not in directory_sizes:
-                    directory_sizes[current_directory] = current_size
-                    current_size = 0
-                directory = parts[2]
-                current_directory = directory_parents[current_directory] if directory == '..' else directory
-        else:
-            if parts[0] == 'dir':
-                directory_parents[parts[1]] = current_directory
+        if parts[1] == 'cd':  # ignore ls
+            if parts[2] == '..':
+                path.pop()
             else:
-                current_size += int(parts[0])
-
-    directory_sizes[current_directory] = current_size
-    for directory, parent in directory_parents.items():
-        directory_sizes[parent] += directory_sizes[directory]
-
-    return directory_sizes
+                path.append(parts[2])
+        elif parts[1] == 'ls':
+            continue
+        elif parts[0] == 'dir':
+            continue
+        else:
+            size = int(parts[0])
+            for i in range(1, len(path) + 1):
+                sizes['/'.join(path[:i])] += size
+    return sizes
 
 
 def main():
     with open('day-7-2022/input-1.txt') as rd:
-        items = [line for line in rd.readlines()]
-
-        directory_sizes = build_map_of_directory_sizes(items)
-        over = 0
-        under = 0
+        items = [line.strip() for line in rd.readlines()]
+        sizes = build_map_of_directory_sizes(items)
+        target_memory = 70000000 - 30000000
+        size_to_free = sizes['/'] - target_memory
+        min_directory_size = 0
         sum = 0
-        for key, value in directory_sizes.items():
+        for key, value in sizes.items():
             if value <= 100000:
-                under += 1
                 sum += value
-            else:
-                over += 1
-        print(sum, over, under)
+            if value >= size_to_free:
+                if min_directory_size == 0:
+                    min_directory_size = value
+                else:
+                    min_directory_size = value if value < min_directory_size else min_directory_size
+        print(sum)
+        print(min_directory_size)
 
 
 if __name__ == "__main__":
---
+++ 2022/day-8-2022/day8.py
@@ -0,0 +1,95 @@
+#! /usr/bin/env python
+import numpy as np
+
+
+def get_grid_peremeter(items):
+    return 2 * (len(items) + len(items[0]) - 2)
+
+
+def visible_from_direction(tree_height, tree_heights):
+    return all(int(height) < int(tree_height) for height in tree_heights)
+
+
+def tree_is_visible(items, i, j):
+    tree_height = items[i][j]
+    visible_left = True
+    visible_right = True
+    visible_up = True
+    visible_down = True
+
+    for row in range(0, i):
+        if items[row][j] >= tree_height:  # one of heights above is visible
+            visible_up = False
+    for row in range(i + 1, len(items)):
+        if items[row][j] >= tree_height:  # one of heights under is visible
+            visible_down = False
+            break
+    for height in items[i][:j]:  # one of heights to the left is visible
+        if height >= tree_height:
+            visible_left = False
+            break
+    for height in items[i][j + 1:]:  # one of heights to the right is visible
+        if height >= tree_height:
+            visible_right = False
+            break
+
+    return visible_left or visible_right or visible_up or visible_down
+
+
+def get_number_of_visible_trees(items):
+    count = 0
+    for i in range(1, len(items) - 1):
+        for j in range(1, len(items[0]) - 1):
+            if tree_is_visible(items, i, j):
+                count += 1
+    return count + get_grid_peremeter(items)
+
+
+def get_scenic_score(items, i, j):
+    tree_height = items[i][j]
+    visible_trees_left = 0
+    visible_trees_right = 0
+    visible_trees_up = 0
+    visible_trees_down = 0
+    for row in range(i - 1, -1, -1):
+        visible_trees_up += 1
+        if items[row][j] >= tree_height: # one of heights above is visible
+            break
+    for row in range(i + 1, len(items)):
+        visible_trees_down += 1
+        if items[row][j] >= tree_height:  # one of heights under is visible
+            break
+    for height in reversed(items[i][:j]):  # one of heights to the left is visible
+        visible_trees_left += 1
+        if height >= tree_height:
+            break
+    for height in items[i][j + 1:]:  # one of heights to the right is visible
+        visible_trees_right += 1
+        if height >= tree_height:
+            break
+
+    return visible_trees_right * visible_trees_left * visible_trees_down * visible_trees_up
+
+
+def get_highest_scenic_score(items):
+    grid = items
+    highest_scenic_score = 0
+    for i in range(1, len(items)):
+        for j in range(1, len(items[0])):
+            scenic_score = get_scenic_score(grid, i, j)
+            if scenic_score > highest_scenic_score:
+                highest_scenic_score = scenic_score
+    return highest_scenic_score
+
+
+def main():
+    with open('day-8-2022/input-1.txt') as rd:
+        items = np.array([[int(l) for l in line.strip()] for line in rd.readlines()])
+        number_of_visible_trees = get_number_of_visible_trees(items)  # part 1
+        highest_scenic_score = get_highest_scenic_score(items)  # part 2
+        print(number_of_visible_trees)
+        print(highest_scenic_score)
+
+
+if __name__ == "__main__":
+    main()
